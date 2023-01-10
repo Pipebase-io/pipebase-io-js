a67ae626425e6ae4a94125418d99144db38adaf0
Ron Cohen <rocohen@microsoft.com>
2023-01-10 19:16:07 +0200

    Rebrand

--- __tests__/Relogs.test.ts
+++ __tests__/Relogs.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import 'jest';
-import { Relogs } from '../src/index';
+import { PipebaseClient } from '../src/index';
 require('dotenv').config()
 
 function sleep(ms: number) {
@@ -9,9 +9,9 @@ function sleep(ms: number) {
 
 
 test('Playground', async () => {
-  const relogs = new Relogs({
-    workspaceId: process.env.RELOGS_TEST_WORKSPACE_ID as string,
-    apiKey: process.env.RELOGS_TEST_API_KEY as string,
+  const pipebaseClient = new PipebaseClient({
+    workspaceId: process.env.PIPEBASE_TEST_WORKSPACE_ID as string,
+    apiKey: process.env.PIPEBASE_TEST_API_KEY as string,
     defaultTable: 'test-js-ingest-8',
     ingestConsoleLogs: true
   });
@@ -21,14 +21,14 @@ test('Playground', async () => {
   console.error(`TEST this will be logged as error `);
   console.log("TEST parameters will also be logged", { hello: "woaaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaa aaaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaa aaaarld" }, { anotherParam: 1 }, 5, 6, 7);
 
-  await relogs.flushAsync(); // Can call flush to upload the logs (logs are flushed every second by default) 
+  await pipebaseClient.flushAsync(); // Can call flush to upload the logs (logs are flushed every second by default) 
 
-  relogs.track({
+  pipebaseClient.track({
     myEvent: "You can log anything you want",
     moreData: 1234
   }, "customTableName");
 
-  await relogs.end(); // Flush and stop logging (will wait for logs to upload for up to 10 seconds)
+  await pipebaseClient.end(); // Flush and stop logging (will wait for logs to upload for up to 10 seconds)
 
-  console.log(`This log will not be uploaded to Relogs`);
+  console.log(`This log will not be uploaded to Pipebase`);
 });
